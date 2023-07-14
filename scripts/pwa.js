6f747b0afdbbbfb99aa01ed382e4b7371a6f8dd0
Presley <tph.presley@gmail.com>
2023-07-14 15:10:32 +0800

    update

--- scripts/pwa.js
+++ scripts/pwa.js
@@ -1,55 +1,26 @@
-function showInstallPromo() {
-  const installPopup = document.getElementById("install-popup");
-  installPopup.style.display = "flex";
-  installPopup.style.justifyContent = "center";
-  installPopup.style.alignItems = "center";
-  installPopup.style.height = "100vh";
-  installPopup.style.background = "#5900b3";
-  // installPopup.style.backgroundImage =
-  //   "url('images/yunshangtong2021412131628/202141214534.jpg')";
-  //   installPopup.style.backgroundSize = "100% auto";
-  // installPopup.style.backgroundSize = "cover";
-
-  const installButton = document.getElementById("install-btn");
-  installButton.addEventListener("click", installPWA);
-
-  // const cancelButton = document.getElementById("cancel-btn");
-  // cancelButton.addEventListener("click", cancelInstall);
-}
-
+// PWA installation flow
+let deferredPrompt;
 function installPWA() {
   if (deferredPrompt) {
     deferredPrompt.prompt();
     deferredPrompt.userChoice.then((choiceResult) => {
-      // if (choiceResult.outcome === "accepted") {
-      //   console.log("PWA installation accepted");
-      // } else {
-      //   console.log("PWA installation dismissed");
-      // }
       deferredPrompt = null;
-      hideInstallPromo();
     });
   }
 }
 
-// function cancelInstall() {
-//   deferredPrompt = null;
-//   hideInstallPromo();
-// }
-
-function hideInstallPromo() {
-  const installPopup = document.getElementById("install-popup");
-  installPopup.style.display = "none";
-
-  const mainContent = document.getElementById("main-content");
-  mainContent.style.display = "block";
-}
-
-let deferredPrompt;
-let installSource;
-
 window.addEventListener("beforeinstallprompt", (e) => {
   e.preventDefault();
   deferredPrompt = e;
-  showInstallPromo();
+  Swal.fire({
+    title: "安装移动应用程序?",
+    showDenyButton: false,
+    showCancelButton: false,
+    confirmButtonText: "安装",
+    denyButtonText: `取消`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      installPWA();
+    }
+  });
 });
